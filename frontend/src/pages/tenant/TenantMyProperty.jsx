73fa5b9b2c2f716eb9544fbc5863ced5b5a738f2
muroyyy <amirul.faiz01@gmail.com>
2025-12-31 17:00:15 +0800

    fix: update tenant property image modal background opacity

--- frontend/src/pages/tenant/TenantMyProperty.jsx
+++ frontend/src/pages/tenant/TenantMyProperty.jsx
@@ -307,7 +307,7 @@ export default function TenantMyProperty() {
 
       {/* Image Modal */}
       {showImageModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center p-4 z-50">
+        <div className="fixed inset-0 bg-black/20 flex items-center justify-center p-4 z-50">
           <div className="relative max-w-4xl max-h-[90vh] w-full">
             <button
               onClick={() => setShowImageModal(false)}
